4bb041484e1a214f501128e9971be110b7d17cbb
emon3455 <emon.mhk69@gmail.com>
2023-02-01 01:43:07 +0600

    removing duplicates from an array

--- 6problem.js
+++ 6problem.js
@@ -1,3 +1,17 @@
 
+function findingMax(arr){
+    let max = arr[0];
 
+    for(let i =0; i< arr.length; i++){
+        if(arr[i]>max){
+            max = arr[i];
+        }
+    }
+
+    return max;
+}
+
+let numbers = [33,1, 2, 3, 3, 4,23, 4,100, 5, 6, 7,44, 8, 9, 10,80];
+let maximum = findingMax(numbers);
+console.log(maximum);
 
---
+++ 7problem.js
@@ -0,0 +1,24 @@
+function removingDuplicates(arr){
+
+    let newArray=[];
+    let check;
+    let element;
+    let x=0;
+
+    for(let i=0; i< arr.length; i++){
+
+        element = arr[i];
+        check = newArray.includes(element);
+        if(check == false){
+            newArray[x]=arr[i];
+            x++;
+        }
+    }
+
+    return newArray;
+}
+
+let arr = [1,2,3,4,5,5,5,5,4,4,3,2,1];
+let newArr = removingDuplicates(arr);
+console.log(newArr);
+
